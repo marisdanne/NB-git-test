c0cc937eda8f61aeab88137ca61f5b9759aef097
superMaris <user@DESKTOP-752HCT9>
2020-01-24 18:17:40 +0200

    Otrais commit

--- src/gitproject/GitProject.java
+++ src/gitproject/GitProject.java
@@ -16,6 +16,7 @@ public class GitProject {
      */
     public static void main(String[] args) {
         System.out.println("Sveika, pasaule!");
+        System.out.println("Sveika, pasaule!");
     }
     
 }
